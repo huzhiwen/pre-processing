9c9445032b9759da965cf15e009f63d3201b8217
Zhiwen Hu <zhiwenhu@Zhiwens-MacBook-Pro-2.local>
2019-06-07 11:49:00 -0700

    data

---
+++ baseline_preprocessing/pre-processing-retweet.py
@@ -0,0 +1,35 @@
+import numpy as np
+from scipy.sparse import csr_matrix
+import random
+import keras.backend as K
+from sklearn import svm
+from sklearn.metrics import accuracy_score
+import math
+import csv
+from sklearn.preprocessing import LabelEncoder
+import pandas as pd
+import networkx as nx
+
+# df = pd.read_csv('data/friend_list.csv',sep="\t")
+# g = nx.from_pandas_edgelist(df, "follower", "followee", edge_attr=True)
+# adjacent_list = nx.to_dict_of_lists(g, nodelist=None)
+# nx.write_adjlist(g,"data/friend.adjlist")
+
+df_label = pd.read_csv('data/origin_data/dict.csv',sep="\t",
+        header=0,
+		usecols=[1,3],
+		names=["twitter_id","party"])
+df_label.replace(('R', 'D'), (0, 1), inplace=True)
+
+
+df = pd.read_csv('data/origin_data/retweet_list.csv',sep="\t")
+g = nx.from_pandas_edgelist(df, "user_id", "retweeted", edge_attr=True)
+df_label = df_label[
+					df_label['twitter_id'].apply(lambda x: x in list(g.nodes))
+  					]
+
+
+print(len(g.nodes))
+print(len(g.edges))
+df_label.to_csv("data/retweet_labels_baseline.txt", header = False, columns = ['twitter_id','party'], sep=' ', index = False)
+df.to_csv("data/retweet_list_baseline.csv", header = False, columns = ['user_id','retweeted'], sep=' ', index = False)
